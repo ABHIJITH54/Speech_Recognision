5648aef7d29296cabac018e0f84a5f6f1ea76371
ABHIJITH54 <jabhijith52@gmail.com>
2025-06-11 09:51:13 +0530

    speech_recognision

---
+++ speech.py
@@ -0,0 +1,22 @@
+import speech_recognition as sr
+
+def recognize_speech_from_microphone():
+    recognizer=sr.Recognizer()
+    mic=sr.Microphone()
+    while True:
+        try:
+            with mic as source:
+                print("please speak something")
+                audio=recognizer.listen(source)
+                print("Recognizing")
+                text=recognizer.recognize_google(audio)
+                print(f"you said: {text}")
+                # return text
+                if text.lower()=="stop":
+                    break
+        except sr.UnknownValueError:
+            print("sorry i did not understand you")
+        except sr.RequestError as e:
+            print(f"An error occured : {e}")
+
+recognize_speech_from_microphone()
